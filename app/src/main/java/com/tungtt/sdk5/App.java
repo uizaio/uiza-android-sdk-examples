e8ee613c180616bb8058ed88d5ab89e8092b1f45
tranthanhtungp501 <34903738+tranthanhtungp501@users.noreply.github.com>
2019-04-23 18:00:39 +0700

    Update App.java

--- app/src/main/java/com/tungtt/sdk5/App.java
+++ app/src/main/java/com/tungtt/sdk5/App.java
@@ -6,9 +6,9 @@ import vn.uiza.core.common.Constants;
 
 public class App extends MultiDexApplication {
     public static final String DF_DoMain_Api = "ap-southeast-1-api.uiza.co";
-    public static final String DF_TOKEN = "uap-b99d6b58428043ffbbc2091054ef3442-dae7e075";
-    public static final String DF_AppId = "b99d6b58428043ffbbc2091054ef3442";
-    public static String entityIdDefaultLIVE_TRANSCODE = "118e2ae0-7462-489d-b884-1d13eb422b48";
+    public static final String DF_TOKEN = "YOUR_TOKEN_API";
+    public static final String DF_AppId = "YOUR_APP_ID";
+    public static String entityIdDefaultLIVE_TRANSCODE = "YOUR_ENTITY_TRANSCODE";
 
     @Override
     public void onCreate() {
